1ca52298bd3aa454e0c1bc768b9cb4f854cbb7a2
AZa0Ra <rostyk.gydzelyak@gmail.com>
2024-09-20 05:19:01 +0300

    Add second varian to lab1

---
+++ Lab1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooap_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(3);
+            circle.PrintArea();
+
+            Shape square = new Square(5);
+            square.PrintArea();
+
+            Shape rectangle = new Rectangle(6, 8);
+            rectangle.PrintArea();
+
+
+
+            
+        }
+    }
+}
---
+++ Lab1_2/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooap_lab1
+{
+    abstract class Shape
+    {
+        public Shape()
+        {
+            Console.WriteLine("Subject was created");
+        }
+
+        public abstract double CalculateArea();
+
+        public void PrintArea()
+        {
+            Console.WriteLine($"Shape square: {CalculateArea()}");
+        }
+
+    }
+}
